7bbfd90b511acbcaeb778c897c684371bb20ca9b
Stanislav German <stas.german@gmail.com>
2020-10-19 13:40:36 +0300

    fix specs

--- app/services/matchmaker.rb
+++ app/services/matchmaker.rb
@@ -2,7 +2,6 @@
 
 class Matchmaker
   def self.find_task(assigment)
-    # binding.pry
     assigment.task = Task.unassigned.first!
   end
 end
--- spec/factories/affiliations_factory.rb
+++ spec/factories/affiliations_factory.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
   factory :affiliation do
     value { 1 }
     task
+    category
 
     trait :dune do
       category { Category.find_or_create_by!(name: 'Dune') }
